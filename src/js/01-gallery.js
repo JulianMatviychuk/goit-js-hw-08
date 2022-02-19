2c1311f207f888c48b03b1e92d380b8ccc027ae8
Julian Matviychuk <julianflavius@gmail.com>
2022-02-19 15:27:21 +0200

    change repos

---
+++ src/js/01-gallery.js
@@ -0,0 +1,31 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+const imageGallery = document.querySelector('.gallery');
+
+renderGallery();
+
+function renderGallery() {
+  const markup = galleryItems
+    .map(
+      ({ preview, original, description }) => `
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+  `
+    )
+    .join('');
+
+  imageGallery.insertAdjacentHTML('beforeend', markup);
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+console.log(galleryItems);
