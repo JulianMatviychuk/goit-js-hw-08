5da5094c0ba8b7d9349b445dfb1f8c0cc877ea8a
Julian Matviychuk <julianflavius@gmail.com>
2022-02-19 19:03:18 +0200

    finish player start feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,19 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+
+console.log(form);
+
+form.addEventListener('input', throttle(onFormInput, 500));
+// form.addEventListener('submit', onFormSubmit);
+
+const storageKey = 'feedback-form-state';
+
+const formData = {};
+
+function onFormInput(event) {
+  formData.message = form.element.message.value;
+  formData.email = form.element.email.value;
+  localStorage.setItem(storageKey, JSON.stringify(formData));
+}
 
